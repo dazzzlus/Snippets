728d423325bea7109e785befaca5d603e2164348
dazzzlus <100034429@edu.miigaik.ru>
2023-07-13 19:07:11 +0300

    first commit

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 from MainApp import views
+
 
 urlpatterns = [
     path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('snippets/add', views.add_snippet_page, name="add_snippets"),
+    path('snippets/list', views.snippets_page, name="snippets_page"),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
