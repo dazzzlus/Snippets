4e8bbb00424e09a70c2757057e1e386e353a8dc6
dazzzlus <100034429@edu.miigaik.ru>
2023-07-14 18:57:29 +0300

    forms and models

---
+++ MainApp/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm, TextInput
+from MainApp.models import Snippet
+
+
+class SnippetForm(ModelForm):
+   class Meta:
+       model = Snippet
+       # Описываем поля, которые будем заполнять в форме
+       fields = ['name', 'lang', 'code']
+       widgets = {
+          'name': TextInput(attrs={"class":"form-control form-control-lg", 'placeholder': 'Название сниппета'}),
+          }
+       labels = {
+          'name': ''
+        }
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
-
+from MainApp.forms import SnippetForm
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -8,10 +8,26 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == 'GET':
+        form = SnippetForm()    
+        context = {'pagename': 'Добавление нового сниппета', 'form':form}
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method=='POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        return render(request, 'pages/add_snippet.html', {'form': form})
 
 
 def snippets_page(request):
     context = {'pagename': 'Просмотр сниппетов'}
     return render(request, 'pages/view_snippets.html', context)
+
+def create_snippet(request):
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('redirect_url')       ###################temporary
+    return render(request, 'add_snippet.html', {'form': form})
